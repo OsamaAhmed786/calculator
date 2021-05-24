5b2c12aa7985161786af4b80dd39c11b8ca85b65
OsamaAhmed786 <84673292+OsamaAhmed786@users.noreply.github.com>
2021-05-24 17:30:37 +0500

    calculator
    this is simple a calculator 
    + , - , * , / .

---
+++ hello.py
@@ -0,0 +1,19 @@
+
+#print('hello world')
+#print('osama')
+
+#a = 2 
+#b = 3
+f_num = int(input('enter ur  First number'))
+s_num = int(input('enter ur  second number'))
+ope = input('operator')
+if ope == '+':
+    print(f_num + s_num)
+elif ope == '-':
+    print(f_num - s_num)
+elif ope == '*':
+    print(f_num * s_num)
+elif ope == '/':
+    print(f_num / s_num)
+        
+#print(a * 2)
